41e2d6957576a7060a560562258d45cbacbeaba9
MiskaPuurunen <miska.puurunen@student.lut.fi>
2026-07-09 16:37:30 +0300

    Create py file

---
+++ SatListnr.py
@@ -0,0 +1,15 @@
+import json
+import queue
+import socket
+import threading
+import time
+import tkinter as tk
+from collections import deque
+from tkinter import ttk, messagebox
+import random
+import math
+
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
